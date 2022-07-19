780161fc961f3c1848a3b1f3514fc7832edbf046
amina-deriv <amina@deriv.com>
2022-07-19 15:08:47 +0400

    svg

--- packages/account/src/Components/poi-poa-docs-submitted/poi-poa-docs-submitted.jsx
+++ packages/account/src/Components/poi-poa-docs-submitted/poi-poa-docs-submitted.jsx
@@ -21,7 +21,7 @@ const PoiPoaSubmitted = ({ onClickOK, account_type, mt5_login_list }) => {
                 <Localize i18n_default_text='You can trade with the SVG account in a single step while waiting. Add the {{type_of_account}} SVG account?'
                     values={{ type_of_account: account_type.type }} />
             </Text>
-            <div className=''>
+            <div className='poi-poa-submitted-svg-footer'>
                 <Button has_effect text={localize('No')} onClick={onClickOK} primary />
                 <Button has_effect text={localize('Yes')} onClick={onClickOK} primary />
             </div>
